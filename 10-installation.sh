2c23173a5dab95019598947b4264a8ddc1965735
ramadevidev6 <ramadevi.dev6@gmail.com>
2024-05-18 22:46:32 +0530

    variable

---
+++ 10-installation.sh
@@ -0,0 +1,40 @@
+#  !/bin/bash
+# you want to install you should have super user access
+# id   , sudo su ,id ,
+
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root access."
+    exit 1 # manually exit if error comes.
+else
+    echo "You are super user."
+fi
+
+dnf install mysql -y  #  to install mysql 
+
+:' if [ $? -ne 0 ]  #  $? means previous command
+then
+    echo "Installation of mysql...FAILURE"
+    exit 1
+else
+    echo "Installation of mysql...SUCCESS"
+fi
+
+dnf install git -y
+
+if [ $? -ne 0 ]
+then
+    echo "Installation of git...FAILURE"
+    exit 1
+else
+    echo "Installation of Git...SUCCESS"
+fi 
+
+echo "is script proceeding?" '
+
+
+
+# : '  multi line comment in shell script '
