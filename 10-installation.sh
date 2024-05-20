c2bbf46e172254b016fc2ae5d2c74200bedc5491
ramadevidev6 <ramadevi.dev6@gmail.com>
2024-05-20 10:24:10 +0530

    variable

--- 10-installation.sh
+++ 10-installation.sh
@@ -1,4 +1,4 @@
-#  !/bin/bash
+#!/bin/bash
 # you want to install you should have super user access
 # id   , sudo su ,id ,
 
@@ -13,5 +13,24 @@ else
     echo "You are super user."
 fi
 
-dnf install mysql -y  #  to install mysql 
+dnf install mysql -y  # 
 
+if [ $? -ne 0 ]
+then
+    echo "Installation of mysql...FAILURE"
+    exit 1
+else
+    echo "Installation of mysql...SUCCESS"
+fi
+
+dnf install git -y
+
+if [ $? -ne 0 ]
+then
+    echo "Installation of git...FAILURE"
+    exit 1
+else
+    echo "Installation of Git...SUCCESS"
+fi
+
+echo "is script proceeding?"
